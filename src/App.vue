17616dc74646883a3c3eed534e0491a870e541f7
Primasbt <91203079+primasbt@users.noreply.github.com>
2023-06-07 23:51:01 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Hero from "./components/Hero.vue";
+import NavigationBar from "./components/NavigationBar.vue";
+</script>
+
+<template>
+  <div>
+    <NavigationBar />
+    <Hero />
+  </div>
+</template>
+<style scoped>
+
+</style>
